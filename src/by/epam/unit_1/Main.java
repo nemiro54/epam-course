483a22d5ebc50bb7b9f7e0829133ee6a8eae5feb
Uladzislau Niamira <nemiro54@gmail.com>
2021-12-01 00:00:50 +0300

    Update .gitignore, add solution for task04-05

--- src/by/epam/unit_1/Main.java
+++ src/by/epam/unit_1/Main.java
@@ -1,41 +1,54 @@
 package by.epam.unit_1;
 
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class Main {
 
     public static void main(String[] args) {
-        task01();
-        task02();
-        task03();
     }
 
-    // найдите значение функции: z = ((a - 3) * b / 2) + c
-    public static void task01() {
-        double a = 2;
-        double b = -8;
-        double c = 12.4;
+    // Task01: Найдите значение функции: z = ((a - 3) * b / 2) + c
 
-        double result = (a - 3) * b / 2 + c;
-        System.out.println(String.format("The result task01 is: %f", result));
+    public static double task01(double a, double b, double c) {
+        return (a - 3) * b / 2 + c;
     }
 
-    // вычислите значение выражения по формуле (все переменные принимают действительные значения):
-    // (b + √(b^2 + 4ac) / 2a - a^3 * c - b^(-2)
-    public static void task02() {
-        double a = 4;
-        double b = -5.4;
-        double c = 10;
+    /* Task02: Вычислите значение выражения по формуле (все переменные принимают действительные значения):
+       (b + √(b^2 + 4ac) / 2a - a^3 * c - b^(-2) */
 
-        double result = ((b + Math.sqrt(Math.pow(b, 2) + 4 * a * c)) / (2 * a) - Math.pow(a, 3) * c - Math.pow(b, -2));
-        System.out.println(String.format("The result task02 is: %f", result));
+    public static double task02(double a, double b, double c) {
+        return ((b + Math.sqrt(Math.pow(b, 2) + 4 * a * c)) / (2 * a) - Math.pow(a, 3) * c - Math.pow(b, -2));
     }
 
-    // вычислить значение выражения по формуле (все переменные принимают действительные значения):
-    // (sin x + cos y) / (cos x - sin y) * tg xy
-    public static void task03() {
-        double radX = Math.toRadians(30);
-        double radY = Math.toRadians(60);
+    /* Task03: Вычислить значение выражения по формуле (все переменные принимают действительные значения):
+       (sin x + cos y) / (cos x - sin y) * tg xy */
 
-        double result = ((Math.sin(radX) + Math.cos(radY)) / Math.cos(radX) - Math.sin(radY)) * Math.tan(radX * radY);
-        System.out.println(String.format("The result task03 is: %f", result));
+    public static double task03(double x, double y) {
+        double radX = Math.toRadians(x);
+        double radY = Math.toRadians(y);
+
+        return ((Math.sin(radX) + Math.cos(radY)) / Math.cos(radX) - Math.sin(radY)) * Math.tan(radX * radY);
+    }
+
+    /* Task04: Дано действительное число R вида nnn.ddd (три цифровых разряда в целой и дробной частях).
+       Поменять местами дробную и целую части числа и вывести полученное значение числа */
+
+    public static void task04(double number) {
+        double reverseNumber = (number * 1000) % 1000 + (int) number / 1000.0;
+        System.out.println(reverseNumber);
+    }
+
+    /* Task05: Дано натуральное число Т, которое представляет длительность прошедшего времени в секундах.
+       Вывести данное значение длительности в часах, минутах и секундах в следующей форме:
+       HHч MMмин SSс */
+
+    public static void task05(int secondValue) {
+        int hours = secondValue / 3600;
+        int tmp = secondValue - hours * 3600;
+        int minutes = tmp / 60;
+        int seconds = tmp - minutes * 60;
+
+        System.out.println(String.format("%dч %dмин %dс", hours, minutes, seconds));
     }
 }
