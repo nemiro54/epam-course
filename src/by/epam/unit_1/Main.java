f3aa6895d5bc3f8bff7d8d2740cc61e561ebfd39
Uladzislau Niamira <nemiro54@gmail.com>
2021-12-03 21:50:25 +0300

    Add solution for task12-17

--- src/by/epam/unit_1/Main.java
+++ src/by/epam/unit_1/Main.java
@@ -1,11 +1,14 @@
 package by.epam.unit_1;
 
+import java.math.BigInteger;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        task17();
     }
     // Task01: Найдите значение функции: z = ((a - 3) * b / 2) + c
 
@@ -15,7 +18,7 @@ public class Main {
 
     /* Task02: Вычислите значение выражения по формуле (все переменные принимают действительные значения):
     (b + √(b^2 + 4ac) / 2a - a^3 * c - b^(-2)
-    */
+     */
 
     static double task02(double a, double b, double c) {
         return ((b + Math.sqrt(Math.pow(b, 2) + 4 * a * c)) / (2 * a) - Math.pow(a, 3) * c - Math.pow(b, -2));
@@ -23,7 +26,7 @@ public class Main {
 
     /* Task03: Вычислить значение выражения по формуле (все переменные принимают действительные значения):
     (sin x + cos y) / (cos x - sin y) * tg xy
-    */
+     */
 
     static double task03(double x, double y) {
         double radX = Math.toRadians(x);
@@ -34,7 +37,7 @@ public class Main {
 
     /* Task04: Дано действительное число R вида nnn.ddd (три цифровых разряда в целой и дробной частях).
     Поменять местами дробную и целую части числа и вывести полученное значение числа
-    */
+     */
 
     static void task04(double number) {
         double reverseNumber = (number * 1000) % 1000 + (int) number / 1000.0;
@@ -44,7 +47,7 @@ public class Main {
     /* Task05: Дано натуральное число Т, которое представляет длительность прошедшего времени в секундах.
     Вывести данное значение длительности в часах, минутах и секундах в следующей форме:
     HHч MMмин SSс
-    */
+     */
 
     static void task05(int secondValue) {
         int hours = secondValue / 3600;
@@ -57,7 +60,7 @@ public class Main {
 
     /* Task06: Для данной области составить линейную программу, которая печатает true, если точка с координатами (x, y)
     принадлежит закрашенной области, и false - в противном случае.
-    */
+     */
 
     static void task06(int x, int y) {
         boolean isFlag = false;
@@ -70,7 +73,7 @@ public class Main {
 
     /* Task07: Даны два угла треугольника (в градусах). Определить, существует ли такой треугольник, и если да,
     то будет ли он прямоугольным.
-    */
+     */
 
     static void task07(int a, int b) {
         if (a + b == 90) {
@@ -100,7 +103,7 @@ public class Main {
 
     /* Task10: Заданы размеры A, B прямоугольного отверстия и размеры x, y, z кирпича.
     Определить пройдет ли кирпич через отверстие.
-    */
+     */
 
     static void task10(int A, int B, int x, int y, int z) {
         if (x <= A && y <= B || y <= A && x <= B ||
@@ -112,7 +115,7 @@ public class Main {
         }
     }
 
-    // Вычислить значение функции.
+    // Task11: Вычислить значение функции.
 
     static void task11(double x) {
         double result;
@@ -123,5 +126,88 @@ public class Main {
             result = 1 / (Math.pow(x, 3) + 6);
         }
         System.out.println(result);
+    }
+
+    /* Task12: Напишите программу, где пользователь вводит любое целое положительное число, а программа
+    суммирует все числа от 1 до введенного пользователем числа.
+     */
+
+    static void task12() {
+        Scanner scanner = new Scanner(System.in);
+        int number;
+        int result = 0;
+
+        while (true) {
+            System.out.println("Enter a positive number");
+            number = scanner.nextInt();
+
+            if (number <= 0) {
+                System.out.print("Error! ");
+            } else {
+                break;
+            }
+        }
+
+        for (int i = 0; i <= number; i++) {
+            result += i;
+        }
+
+        System.out.println(String.format("Sum is: %d", result));
+    }
+
+    //Task13: Вычислить значение функции y = {x, x > 2; y = -x, x <=2} на отрезке [a, b] с шагом h.
+
+    static void task13(double a, double b, double h) {
+        double y;
+
+        for (double x = a; x <= b; x += h) {
+            if (x > 2) {
+                y = x;
+            } else {
+                y = -x;
+            }
+        }
+    }
+
+    // Task14: Найти сумму квадратов первых ста чисел
+
+    static void task14() {
+        int result = 0;
+
+        for (int i = 0; i <= 100; i++) {
+            result += Math.pow(i, 2);
+        }
+    }
+
+    // Task15: Составить программу нахождения произведения квадратов первых двухсот чисел
+
+    static void task15() {
+        BigInteger result = BigInteger.valueOf(1);
+
+        for (int i = 1; i <= 200; i++) {
+            result = result.multiply(BigInteger.valueOf((long) Math.pow(i, 2)));
+        }
+    }
+
+    /* Task16: Даны числовой ряд и некоторое число е. Найти сумму тех членов ряда, модуль которых больше или равен
+    заданному е. Общий член ряда имеет вид: a = 1 / 2^n + 1 / 3^n
+     */
+
+    static void task16(double e, double n) { // n - первый член ряда
+        double sum = 0;
+
+        while (Math.abs(1 / Math.pow(2, n) + 1 / Math.pow(3, n)) >= e) {
+            sum += Math.abs(1 / Math.pow(2, n) + 1 / Math.pow(3, n));
+            n++;
+        }
+    }
+
+    // Task17: Вывести на экран соответствие между символами и их численными обозначениями в памяти компьютера
+
+    static void task17() {
+        for (int i = 0; i < 256; i++) {
+            char c = (char) i;
+            System.out.println(i + ": " + c);
+        }
     }
 }
