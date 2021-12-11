ab7304bf09d92e9b274ac61d1cd1290b15f14b1e
Uladzislau Niamira <nemiro54@gmail.com>
2021-12-11 16:45:20 +0300

    Add solution for task07-15

--- src/by/epam/unit_2/Main.java
+++ src/by/epam/unit_2/Main.java
@@ -1,5 +1,7 @@
 package by.epam.unit_2;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
     }
@@ -112,5 +114,148 @@ public class Main {
             }
         }
         return countPrimeNumbers;
+    }
+
+    // task07: Даны действительные числа a1, a1, ...an. Найти max(a1 + a2n, a2 + a2n-1, ..., an + an+1).
+
+    static double task07(double[] numbers) {
+        double max = numbers[0] + numbers[numbers.length - 1];
+
+        for (int i = 0; i < numbers.length / 2; i++) {
+            max = Math.max(max, (numbers[i] + numbers[numbers.length - i - 1]));
+        }
+        return max;
+    }
+
+    /* task08: Дана последовательность целых чисел a1, a2, ..., an. Образовать новую последовательность, выбросив
+    из исходной те члены, которые равны min(a1, a2, ..., an).
+     */
+
+    static int[] task08(int[] numbers) {
+        int min = numbers[0];
+        int countMin = 0; // кол-во элементов == min
+
+        for (int element : numbers) {
+            min = Math.min(min, element);
+        }
+        for (int element : numbers) {
+            if (element == min) {
+                countMin++;
+            }
+        }
+
+        int[] newNumbers = new int[numbers.length - countMin];
+        int indexNewArray = 0;
+
+        for (int element : numbers) {
+            if (element != min) {
+                newNumbers[indexNewArray++] = element;
+            }
+        }
+        return newNumbers;
+    }
+
+    /* task09: В массиве целых чисел с количеством элементов n определить наиболее часто встречающееся число.
+    Если таких чисел несколько, то определить наименьшее из них.
+     */
+
+    static int task09(int[] array) {
+        int resultNumber = array[0]; // возвращаемое значение
+        int[] popularNumbers = new int[array.length];
+
+        for (int i = 0; i < array.length; i++) { //  проверка на количество повторений чисел
+            for (int j = 0; j < array.length; j++) {
+                if (array[i] == array[j]) {
+                    popularNumbers[i]++;
+                }
+            }
+        }
+
+        int mostPopularNumber = popularNumbers[0]; // наиболее часто встречающееся число
+        for (int i = 0; i < popularNumbers.length; i++) {
+            if (popularNumbers[i] > mostPopularNumber) {
+                mostPopularNumber = popularNumbers[i];
+                resultNumber = array[i];
+            }
+            if ((mostPopularNumber == popularNumbers[i]) && (array[i] < resultNumber)) {
+                resultNumber = array[i];
+            }
+        }
+        return resultNumber;
+    }
+
+    /* task10: Дан целочисленный массив с количеством элементов n. Сжать массив, выбросив из него каждый второй
+    элемент (освободившиеся элементы заполнить нулями). Примечание. Дополнительный массив не использовать.
+     */
+
+    static int[] task10(int[] array) {
+        for (int i = 0; i < array.length / 2; i ++) {
+            array[i] = array[i * 2];
+        }
+        Arrays.fill(array, array.length / 2, array.length - 1, 0);
+        return array;
+    }
+
+    // task11: Дана матрица. Вывести на экран все нечетные столбцы, у которых первый элемент больше последнего.
+
+    static void task11(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j += 2) {
+                if (matrix[0][j] > matrix[matrix.length - 1][j]) {
+                    System.out.print(matrix[i][j] + "\t");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    // task12: Дана квадратная матрица. Вывести на экран элементы, стоящие на диагонали.
+
+    static void task12(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            System.out.println(matrix[i][i]);
+        }
+    }
+
+    // task13: Дана матрица. Вывести k-ю строку и p-й столбец матрицы.
+
+    static void task13(int[][] matrix, int k, int p) {
+        for (int i = 0; i < matrix[k - 1].length; i++) {
+            System.out.print(matrix[k - 1][i] + "\t");
+        }
+        System.out.println();
+        for (int i = 0; i < matrix.length; i++) {
+            System.out.println(matrix[i][p - 1]);
+        }
+    }
+
+    // task14: Сформировать квадратную матрицу порядка n по заданному образцу (n - четное).
+
+    static int[][] task14(int n) {
+        int[][] matrix = new int[n][n];
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                if (i % 2 == 0) {
+                    matrix[i][j] = j + 1;
+                } else {
+                    matrix[i][j] = n - j;
+                }
+            }
+        }
+        return matrix;
+    }
+
+    // task15: Сформировать квадратную матрицу порядка n по заданному образцу (n - четное).
+
+    static int[][] task15(int n) {
+        int[][] matrix = new int[n][n];
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length - i; j++) {
+                matrix[i][j] = i + 1;
+            }
+        }
+        return matrix;
     }
 }
