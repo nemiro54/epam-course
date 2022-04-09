5c114f72f2ad79f47488613eed6d123d71e3225f
Uladzislau Niamira <nemiro54@gmail.com>
2022-04-09 18:06:12 +0300

    Add the solution for task 14-15

---
+++ src/by/epam/unit_3/Task14.java
@@ -0,0 +1,48 @@
+package by.epam.unit_3;
+
+/*
+task14: Посчитать количество строчных (маленьких) и прописных (больших) букв во введенной строке.
+Учитывать только английские буквы.
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Task14 {
+    public static void main(String[] args) {
+        System.out.println(countLetters());
+    }
+
+    static String countLetters() {
+        int capitalLetters;
+        int smallLetters;
+        String str;
+
+        try(BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in))) {
+            str = bufferedReader.readLine();
+
+            capitalLetters = countCapitalLetters(str);
+
+            smallLetters = countSmallLetters(str);
+
+            return String.format("Строчных букв: %d \nПрописных букв: %d", smallLetters, capitalLetters);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    static int countCapitalLetters(String str) {
+        return (int) str.chars()
+                .filter(Character::isUpperCase)
+                .count();
+    }
+
+    static int countSmallLetters(String str) {
+        return (int) str.chars()
+                .filter(Character::isLowerCase)
+                .count();
+    }
+}
---
+++ src/by/epam/unit_3/Task15.java
@@ -0,0 +1,29 @@
+package by.epam.unit_3;
+
+/*
+task15: Строка Х состоит из нескольких предложений, каждое из которых кончается точкой, восклицательным или
+вопросительным знаком. Определить количество предложений в стоке Х.
+ */
+
+public class Task15 {
+    public static void main(String[] args) {
+        System.out.println(countSentences(
+                "Hello, world. " +
+                "This is my first year learning english and java! " +
+                "I'm really happy." +
+                "Will I be able to finish my journey and get a job offer this year? " +
+                        "How do you think?"));
+    }
+
+    static int countSentences(String str) {
+        int count = 0;
+
+        for (char c : str.toCharArray()) {
+            if (c == '.' || c == '!' || c == '?') {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
